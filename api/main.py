e0ed829596e51e781d047e4554cab2daefba57f5
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-09-17 17:34:20 -0300

    Get top 20 and remove currency

--- api/main.py
+++ api/main.py
@@ -9,17 +9,34 @@ app = Flask(__name__)
 db_connection = get_db_connection('mongodb://localhost:27017')
 
 def get_documents():
-	params ={}
-	name = int(request.args.get('name',''))
-	limit = int(request.args.get('limit', 0))
-	if name:
-		params.update({'name':name})
-	cursor=db_connection.tickers.find(
-		params,{'_id':0,'ticker_hash':0}).limit(limit)
-	return list(cursor)
+    params ={}
+    name = int(request.args.get('name',''))
+    limit = int(request.args.get('limit', 0))
+    if name:
+      params.update({'name':name})
+    cursor=db_connection.tickers.find(
+      params,{'_id':0,'ticker_hash':0}).limit(limit)
+    return list(cursor)
 
 def get_top20():
-	pass
+    params = {}
+    name = int(request.args.get('name', ''))
+    limit = int(request.args.get('limit', 0))
+    if name:
+      params.update({'name':name})
+    params.update({'rank': {'$lte': 20}})
+    cursor = db_connection.tickers.find(
+      params, {'_id': 0, 'ticker_hash': 0}
+    ).limit(limit)
+    return list(cursor)
 
 def remove_currency():
-	pass
+    params = {}
+    name = request.args.get('name', '')
+    if name:
+        params.update({'name': name})
+    else:
+        return False
+    return db_connection.tickers.delete_many(
+            params
+    ).deleted_count
