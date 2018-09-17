fef00a679d0a4978b947ab6f14d7bbb588290a15
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-09-17 16:34:32 -0300

    Save collection tickers

--- agent/main.py
+++ agent/main.py
@@ -18,3 +18,27 @@ def get_cryptocurrencies_from_api():
         return result
 
     raise Exception('API Error')
+
+# Check Availability
+def check_if_exists(db_connection, ticker_data):
+    if db_connection.tickers.find_one({'ticker_hash': 's'}):
+        return True
+
+    return False
+
+# Save ticket
+def save_ticker(db_connection, ticker_data = None):
+    if not ticker_data:
+        return False
+
+    if check_if_exists(db_connection, ticker_data):
+        return False
+
+    # Convert to integers
+    ticker_data['rank'] = int(ticker_data['rank'])
+    ticker_data['last_updated'] = int(ticker_data['last_updated'])
+
+    # Insert a document into the MongoDB collection
+    db_connection.tickers.insert_one(ticker_data)
+    return True
+    
